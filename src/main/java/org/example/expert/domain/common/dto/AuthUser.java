3b5f9b88461c8e34d40207d6667d13cff22c5c74
WooLamLee <98564214+w00lam@users.noreply.github.com>
2026-06-15 19:52:17 +0900

    feat: 사용자 닉네임 인증 흐름 추가

--- src/main/java/org/example/expert/domain/common/dto/AuthUser.java
+++ src/main/java/org/example/expert/domain/common/dto/AuthUser.java
@@ -1,18 +1,21 @@
 package org.example.expert.domain.common.dto;
 
 import lombok.Getter;
+
 import org.example.expert.domain.user.enums.UserRole;
 
 @Getter
 public class AuthUser {
 
-    private final Long id;
-    private final String email;
-    private final UserRole userRole;
+	private final Long id;
+	private final String email;
+	private final UserRole userRole;
+	private final String nickname;
 
-    public AuthUser(Long id, String email, UserRole userRole) {
-        this.id = id;
-        this.email = email;
-        this.userRole = userRole;
-    }
+	public AuthUser(Long id, String email, UserRole userRole, String nickname) {
+		this.id = id;
+		this.email = email;
+		this.userRole = userRole;
+		this.nickname = nickname;
+	}
 }
