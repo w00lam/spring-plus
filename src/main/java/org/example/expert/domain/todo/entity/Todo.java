cdf36683d1bdcd33988aa537593144cddc99ed8f
WooLamLee <98564214+w00lam@users.noreply.github.com>
2026-06-16 13:01:49 +0900

    fix: Todo 생성 시 Manager 저장 누락 문제 수정

--- src/main/java/org/example/expert/domain/todo/entity/Todo.java
+++ src/main/java/org/example/expert/domain/todo/entity/Todo.java
@@ -3,6 +3,7 @@ package org.example.expert.domain.todo.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
 import org.example.expert.domain.comment.entity.Comment;
 import org.example.expert.domain.common.entity.Timestamped;
 import org.example.expert.domain.manager.entity.Manager;
@@ -17,27 +18,28 @@ import java.util.List;
 @Table(name = "todos")
 public class Todo extends Timestamped {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String title;
-    private String contents;
-    private String weather;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String title;
+	private String contents;
+	private String weather;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id", nullable = false)
-    private User user;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", nullable = false)
+	private User user;
 
-    @OneToMany(mappedBy = "todo", cascade = CascadeType.REMOVE)
-    private List<Comment> comments = new ArrayList<>();
+	@OneToMany(mappedBy = "todo", cascade = CascadeType.REMOVE)
+	private List<Comment> comments = new ArrayList<>();
 
-    @OneToMany(mappedBy = "todo")
-    private List<Manager> managers = new ArrayList<>();
+	@OneToMany(mappedBy = "todo", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<Manager> managers = new ArrayList<>();
 
-    public Todo(String title, String contents, String weather, User user) {
-        this.title = title;
-        this.contents = contents;
-        this.weather = weather;
-        this.user = user;
-        this.managers.add(new Manager(user, this));
-    }
+	public Todo(String title, String contents, String weather, User user) {
+		this.title = title;
+		this.contents = contents;
+		this.weather = weather;
+		this.user = user;
+		this.managers.add(new Manager(user, this));
+	}
 }
